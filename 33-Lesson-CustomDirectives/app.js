2c719d8b02d58f4dffc22b305aaf0709aff367bb
DESKTOP-54DOS0H\OLIVA <jp.olivadoti@gmail.com>
2017-02-18 14:57:51 +0100

    Lesson 33 - Directives

---
+++ 33-Lesson-CustomDirectives/app.js
@@ -0,0 +1,60 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+
+  $routeProvider
+
+    .when('/', {
+      templateUrl: 'pages/main.html',
+      controller: 'mainController'
+    })
+
+    .when('/second', {
+      templateUrl: 'pages/second.html',
+      controller: 'secondController'
+    })
+
+    .when('/second/:num', {
+      templateUrl: 'pages/second.html',
+      controller: 'secondController'
+    })
+
+}])
+
+myApp.service('nameService', function(){
+
+  var self = this;
+
+  this.name = 'John Doe';
+
+  this.namelength = function() {
+
+    return self.name.length;
+
+  };
+
+});
+
+myApp.controller('mainController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
+
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService) {
+
+
+
+}]);
+
+myApp.directive('searchResult', [function () {
+  return {
+    template: `
+        <a href="#" class="list-group-item">
+          <h4 class="list-group-item-heading">John Doe</h4>
+          <p class="list-group-item-text">
+            555 Main St., New York, NY 11111
+          </p>
+        </a>
+        `
+  };
+}])
